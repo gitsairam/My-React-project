4a6f28f7b0172e4eb4e6756cd925c6726af1d019
gitsairam <sairamvenkata194@gmail.com>
2021-06-30 15:04:42 +0530

    auth init

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
 
 class GoogleAuth extends Component {
+  componentDidMount() {
+    window.gapi.load('client:auth2', () => {
+      window.gapi.client.init({
+        clientId:
+          '616764956428-m8qd31rlu62qceviphhb55r2ovhod9i6.apps.googleusercontent.com',
+        scope: 'email'
+      });
+    });
+  }
   render() {
     return <div>Google </div>;
   }
