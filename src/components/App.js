cf35b1393d8705b76fea1ea66ad5fbd6c1d23916
gitsairam <sairamvenkata194@gmail.com>
2021-06-30 11:07:43 +0530

    test

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 
 const PageOne = () => {
   return <div>PageOne</div>;
 };
 const PageTwo = () => {
-  return <div>PageTwo</div>;
+  return (
+    <div>
+      PageTwo <Link to="/">Go back</Link>
+    </div>
+  );
 };
 const App = () => {
   return (
     <BrowserRouter>
       <div>
-        <Route path="/" component={PageOne} />
+        <Route path="/" exact component={PageOne} />
         <Route path="/page2" component={PageTwo} />
       </div>
     </BrowserRouter>
