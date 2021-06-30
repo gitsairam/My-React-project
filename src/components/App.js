a0920e622b4f6a92e21ab111abb6f953415fa63a
gitsairam <sairamvenkata194@gmail.com>
2021-06-30 10:53:22 +0530

    1 file modified

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,20 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+
+const PageOne = () => {
+  return <div>PageOne</div>;
+};
+const PageTwo = () => {
+  return <div>PageTwo</div>;
+};
 const App = () => {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <div>
+        <Route path="/" component={PageOne} />
+        <Route path="/page2" component={PageTwo} />
+      </div>
+    </BrowserRouter>
+  );
 };
 export default App;
