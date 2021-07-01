be97f89b03129e8383f7c10768357280ed1527ec
gitsairam <sairamvenkata194@gmail.com>
2021-07-01 12:10:05 +0530

    3 files modified, 1 added

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -13,51 +13,58 @@ class GoogleAuth extends Component {
         })
         .then(() => {
           this.auth = window.gapi.auth2.getAuthInstance();
+
           this.onAuthChange(this.auth.isSignedIn.get());
-          this.auth.isSignedIn.listen(this.onAuthChange());
+          this.auth.isSignedIn.listen(this.onAuthChange);
         });
     });
   }
 
   onAuthChange = isSignedIn => {
     if (isSignedIn) {
-      this.props.signIn();
+      this.props.signIn(this.auth.currentUser.get().getId());
     } else {
       this.props.signOut();
     }
   };
-  onsignInClick() {
+
+  onSignInClick = () => {
     this.auth.signIn();
-  }
-  onsignOutClick() {
+  };
+
+  onSignOutClick = () => {
     this.auth.signOut();
-  }
+  };
+
   renderAuthButton() {
     if (this.props.isSignedIn === null) {
       return null;
     } else if (this.props.isSignedIn) {
       return (
-        <button className="ui red google button" onClick={this.onsignOutClick}>
+        <button onClick={this.onSignOutClick} className="ui red google button">
           <i className="google icon" />
           Sign Out
         </button>
       );
     } else {
       return (
-        <button className="ui red google button" onClick={this.onsignInClick}>
+        <button onClick={this.onSignInClick} className="ui red google button">
           <i className="google icon" />
           Sign In with Google
         </button>
       );
     }
   }
+
   render() {
-    return <div>{this.renderAuthButton()} </div>;
+    return <div>{this.renderAuthButton()}</div>;
   }
 }
+
 const mapStateToProps = state => {
   return { isSignedIn: state.auth.isSignedIn };
 };
+
 export default connect(
   mapStateToProps,
   { signIn, signOut }
